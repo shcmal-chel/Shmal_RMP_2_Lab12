ecf5518495ecc448798a15b61995f9259315e768
Ivan <shcmalivan@gmail.com>
2026-01-28 22:40:29 +0300

    add delegation user profile

---
+++ src/example/UserProfile.kt
@@ -0,0 +1,37 @@
+package example
+import kotlin.properties.Delegates
+
+class UserProfile(initialName: String, initialEmail: String){
+    var name: String by Delegates.observable(initialName){
+        _, old, new->
+        println("Имя изменено: '$old' -> '$new'")
+    }
+    var email: String by Delegates.observable(initialEmail){
+        _, old, new ->
+        println("Email обновлен: '$old' -> '$new'")
+    }
+    var avatar: String by lazy{
+        println("Загружаем аватар для $name...")
+        "avatar_of_$name.png"
+    }
+}
+
+fun main(){
+    println("Создаем профиль пользователя...")
+    val user = UserProfile("Алиса","alice@example.com")
+
+    println("\nИмя: ${user.name}")
+    println("Email: ${user.email}")
+
+    println("\nОбращаемся к аватару впервые:")
+    println("Файл аватара: ${user.avatar}")
+
+    println("\nОбращаемся к аватару снова (должен быть взят из кэша):")
+    println("Файл аватара: ${user.avatar}")
+
+    println("\nМеняем email:")
+    user.email = "alice_new@example.org"
+
+    println("\nМеняем имя:")
+    user.name = "Алиса К."
+}
